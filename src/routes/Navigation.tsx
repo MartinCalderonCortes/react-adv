5bfc8475100d3487130a7fea4473fff631ce6aba
MartinCortes <mh.straus@hotmail.com>
2022-06-14 16:00:27 -0500

    Componentes Lazy agregados

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -7,18 +7,20 @@ import {
   Routes,
 } from "react-router-dom";
 
+import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyLoad/pages";
+
 import logo from "../logo.svg";
 
 export const Navigation = () => {
   return (
     <BrowserRouter>
       <div className="main-layout">
-        <nav>
+        <nav style={{ overflow: "hidden" }}>
           <img src={logo} alt="React Logo" />
           <ul>
             <li>
               <NavLink
-                to="/home"
+                to="/lazy1"
                 className={({ isActive }) => (!isActive ? "nav-active" : "")}
               >
                 Home
@@ -26,7 +28,7 @@ export const Navigation = () => {
             </li>
             <li>
               <NavLink
-                to="/about"
+                to="/lazy2"
                 className={({ isActive }) => (!isActive ? "nav-active" : "")}
               >
                 About
@@ -34,7 +36,7 @@ export const Navigation = () => {
             </li>
             <li>
               <NavLink
-                to="/users"
+                to="/lazy3"
                 className={({ isActive }) => (!isActive ? "nav-active" : "")}
               >
                 Users
@@ -43,10 +45,10 @@ export const Navigation = () => {
           </ul>
         </nav>
         <Routes>
-          <Route path="about" element={<h1>About Page</h1>} />
-          <Route path="users" element={<h1>Users Page</h1>} />
-          <Route path="home" element={<h1>Home Page</h1>} />
-          <Route path="/*" element={<Navigate to="/home" replace />} />
+          <Route path="lazy1" element={<LazyPage1 />} />
+          <Route path="lazy2" element={<LazyPage2 />} />
+          <Route path="lazy3" element={<LazyPage3 />} />
+          <Route path="/*" element={<Navigate to="/lazy1" replace />} />
         </Routes>
       </div>
     </BrowserRouter>
